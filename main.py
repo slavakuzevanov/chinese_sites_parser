cdb69fc55c7d072e80601b1ffbaab7fcb6065113
Кузеванов Вячеслав Олегович <vyacheslav.kuzevanov@rt.ru>
2021-09-26 19:54:53 +0300

    asynchronous request for each article, but sequential retrieval of links to articles

--- main.py
+++ main.py
@@ -4,29 +4,11 @@ import re
 import urllib.parse
 import asyncio
 import aiohttp
-
-SPACECHINA_URL = 'http://www.spacechina.com/n25/index.html'
-
-HEADERS = {
-    'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/92.0.4515.159 Safari/537.36'
-}
-
-s = requests.Session()
+import time
 
 
 def create_full_link(start_url, list_of_links):
     return [urllib.parse.urljoin(start_url, link) for link in list_of_links]
-
-
-# Получаю html разметку главной страницы сайта
-main_page = s.get(SPACECHINA_URL, headers=HEADERS)
-main_page.encoding = main_page.apparent_encoding
-main_page_soup = BeautifulSoup(main_page.text, 'lxml')
-
-# Получаю html разметку второго меню главной страницы (Центр новостей) и получаю полные ссылки на секции
-menu2 = main_page_soup.find('div', attrs={"class": "common_wrap", "id": "menua2"})
-all_sections_links = [tag['href'] for tag in menu2.find_all('a')]
-full_all_section_links = create_full_link(SPACECHINA_URL, all_sections_links)
 
 
 # Получаю html разметку секции
@@ -75,53 +57,68 @@ def get_full_all_article_links_for_section(section_soup, section_url):
     return full_all_article_links_for_section
 
 
-# Асинхронная функция для поиска ключевых слов в статье
-async def article_with_key_words_for_section(session, section, article):
-    async with session.get(article, headers=HEADERS) as resp:
-        resp_text = await resp.text()
-        if re.search(r'(航天211厂)|(航天科技一院211厂北京装备公司)|(航天科技一院211厂)|(该厂民品子公司长征火箭装备公司的)|(民品子公司天津技术公司)|(天津技术公司)|(211厂军民融合)|('
+SPACECHINA_URL = 'http://www.spacechina.com/n25/index.html'
+
+HEADERS = {
+    'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/92.0.4515.159 Safari/537.36'
+}
+
+articles = ['http://www.spacechina.com/n25/n2014789/n2014804/index.html',
+            'http://www.spacechina.com/n25/n2014789/n2014799/index.html']
+
+start_time = time.time()
+
+s = requests.Session()
+# Получаю html разметку главной страницы сайта
+main_page = s.get(SPACECHINA_URL, headers=HEADERS)
+main_page.encoding = main_page.apparent_encoding
+main_page_soup = BeautifulSoup(main_page.text, 'lxml')
+
+# Получаю html разметку второго меню главной страницы (Центр новостей) и получаю полные ссылки на секции
+menu2 = main_page_soup.find('div', attrs={"class": "common_wrap", "id": "menua2"})
+all_sections_links = [tag['href'] for tag in menu2.find_all('a')]
+full_all_section_links = create_full_link(SPACECHINA_URL, all_sections_links)
+
+SECTIONS_DICT = dict()
+SECTIONS_DICT_W_KEY_WORDS = dict()
+
+print('SECTIONS:')
+print(full_all_section_links)
+
+for section in full_all_section_links:
+    section_soup = get_section_soup(section_url=section, headers=HEADERS)
+    full_all_article_links_for_section = get_full_all_article_links_for_section(section_soup, section)
+    SECTIONS_DICT[section] = full_all_article_links_for_section
+    SECTIONS_DICT_W_KEY_WORDS[section] = []
+
+
+async def get_page_data(session, section, article):
+    print(article)
+    try:
+        async with session.get(article, headers=HEADERS) as resp:
+            resp_text = await resp.text()
+            if re.search(r'(航天211厂)|(航天科技一院211厂北京装备公司)|(航天科技一院211厂)|(该厂民品子公司长征火箭装备公司的)|(民品子公司天津技术公司)|(天津技术公司)|(211厂军民融合)|('
                      r'长二F火箭)|(天宫一号)|(神舟八)|(神舟九)|(神舟十号)|(长征二F)|(長征五号)|(長征七号)|(長征八号)', resp_text):
-            print(f'found key words in {article}')
-            SECTIONS_DICT[section].append(article)
+                print(f'found key words in {article}')
+                SECTIONS_DICT_W_KEY_WORDS[section].append(article)
+    except:
+        pass
+        # return resp_text
 
 
 async def load_site_data(sections):
     async with aiohttp.ClientSession() as session:
         tasks = []
-        for section_url in sections:
-            SECTIONS_DICT[section_url] = []
-            section_soup = get_section_soup(section_url=section_url, headers=HEADERS)
-            full_all_article_links_for_section = get_full_all_article_links_for_section(section_soup, section_url)
-            for article in full_all_article_links_for_section:
-                task = asyncio.create_task(article_with_key_words_for_section(session, section_url, article))
+        for section in sections:
+            for article in SECTIONS_DICT[section]:
+                task = asyncio.create_task(get_page_data(session, section, article))
                 tasks.append(task)
         await asyncio.gather(*tasks)
 
 
 
+asyncio.run(load_site_data(full_all_section_links))
+end_time = time.time() - start_time
+print(f'Execution time: {end_time} seconds')
 
-# Теперь смотрю есть ли в статьях ключевые слова
-def get_article_links_where_found(list_of_links):
-    article_links_where_found = []
-    count = 0
-    for link in list_of_links:
-        count += 1
-        try:
-            response = s.get(link, headers=HEADERS)
-            response.encoding = response.apparent_encoding
-            if re.search(
-                    r'(航天211厂)|(航天科技一院211厂北京装备公司)|(航天科技一院211厂)|(该厂民品子公司长征火箭装备公司的)|(民品子公司天津技术公司)|(天津技术公司)|(211厂军民融合)|('
-                    r'长二F火箭)|(天宫一号)|(神舟八)|(神舟九)|(神舟十号)|(长征二F)|(長征五号)|(長征七号)|(長征八号)',
-                    response.text):
-                article_links_where_found.append(link)
-            print(f'{count}/{len(list_of_links)}')
-        except:
-            print(f'{count}/{len(list_of_links)}')
-    return article_links_where_found
-
-
-SECTIONS_DICT = dict()
-# Проделываю для каждой секции
-asyncio.run(load_site_data(full_all_section_links[2:3]))
-
-print(SECTIONS_DICT)
+print(SECTIONS_DICT_W_KEY_WORDS)
