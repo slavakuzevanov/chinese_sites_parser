fdf87ded18cf71d946ce510570f210304dea7b37
Vyacheslav Kuzevanov <slava.kuzevanov@mail.ru>
2021-10-25 20:43:25 +0300

    deleted everything unnecessary

--- parser_classes.py
+++ parser_classes.py
@@ -5,7 +5,14 @@ import urllib.parse
 import asyncio
 import aiohttp
 import time
-import json
+import pandas as pd
+import random
+
+proxy_list = ['http://20.81.106.180:8888', 'http://202.73.51.234:80', 'http://51.195.201.93:80',
+              'http://103.152.35.245:80', 'http://104.233.204.77:82', 'http://104.233.204.73:82',
+              'http://209.141.55.228:80', 'http://206.253.164.122:80', 'http://139.99.237.62:80',
+              'http://51.195.201.93:80', 'http://157.119.234.50:80', 'http://202.73.51.234:80',
+              'http://199.19.226.12:80', 'http://103.216.103.25:80', 'http://45.199.148.4:80']
 
 
 class SpaceChinaParser:
@@ -19,6 +26,7 @@ class SpaceChinaParser:
         self.SECTIONS_PAGES_DICT = dict()
         self.SECTIONS_DICT = dict()
         self.SECTIONS_DICT_W_KEY_WORDS = dict()
+        self.ARTICLES_URLS = []
 
     def __create_full_link(self, start_url, list_of_links):
         return [urllib.parse.urljoin(start_url, link) for link in list_of_links]
@@ -89,29 +97,28 @@ class SpaceChinaParser:
                     tasks.append(task)
             await asyncio.gather(*tasks)
 
-    async def __find_article_with_key_words(self, session, section, article):
+    async def __find_article_with_key_words(self, session, section, article, key_words_string):
         print(article)
         try:
             async with session.get(article, headers=self.HEADERS) as resp:
                 resp_text = await resp.text()
-                if re.search(
-                        r'(航天211厂)|(航天科技一院211厂北京装备公司)|(航天科技一院211厂)|(该厂民品子公司长征火箭装备公司的)|(民品子公司天津技术公司)|(天津技术公司)|(211厂军民融合)|('
-                        r'长二F火箭)|(天宫一号)|(神舟八)|(神舟九)|(神舟十号)|(长征二F)|(長征五号)|(長征七号)|(長征八号)', resp_text):
+                if re.search(key_words_string, resp_text):
                     print(f'found key words in {article}')
-                    self.SECTIONS_DICT_W_KEY_WORDS[section].append(article)
+                    self.ARTICLES_URLS.append(article)
         except:
             pass
 
-    async def __load_articles_with_key_words(self, sections):
+    async def __load_articles_with_key_words(self, sections, key_words_string):
         async with aiohttp.ClientSession() as session:
             tasks = []
             for section in sections:
                 for article in self.SECTIONS_DICT[section]:
-                    task = asyncio.create_task(self.__find_article_with_key_words(session, section, article))
+                    task = asyncio.create_task(
+                        self.__find_article_with_key_words(session, section, article, key_words_string))
                     tasks.append(task)
             await asyncio.gather(*tasks)
 
-    def run(self):
+    def run(self, key_words_string):
 
         with requests.Session() as s:
             # Получаю разметку главной страницы
@@ -131,18 +138,147 @@ class SpaceChinaParser:
 
             start_time = time.time()
             asyncio.run(self.__load_pages_for_sections(all_sections_links))
-            print('SECTIONS_PAGES_DICT: ', self.SECTIONS_PAGES_DICT)
             asyncio.run(self.__load_articles_from_first_page_in_sections(all_sections_links))
-            print('------------------------------------------------------------------------------')
-            print(self.SECTIONS_DICT)
             asyncio.run(self.__load_articles_for_sections(all_sections_links))
-            print('|||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||')
-            print(self.SECTIONS_DICT)
-            asyncio.run(self.__load_articles_with_key_words(all_sections_links))
-            print('///////////////////////////////////////////////////////////////////////////////')
-            print(self.SECTIONS_DICT_W_KEY_WORDS)
+            asyncio.run(self.__load_articles_with_key_words(all_sections_links, key_words_string))
 
-            with open('China Aerospace Science and Technology Corporation.json', 'w') as fp:
-                json.dump(self.SECTIONS_DICT_W_KEY_WORDS, fp)
+            pd.DataFrame(self.ARTICLES_URLS, columns=['url']).to_csv(
+                'China Aerospace Science and Technology Corporation.csv',
+                index=False)
+            end_time = time.time() - start_time
+            print(f'Execution time: {end_time} seconds')
+
+
+class jqkaParser:
+    def __init__(self):
+        self.list_of_section_urls = ['http://yuanchuang.10jqka.com.cn/ycall_list/',
+                                'http://invest.10jqka.com.cn/lczx_list/',
+                                'http://news.10jqka.com.cn/today_list/',
+                                'http://stock.10jqka.com.cn/companynews_list/',
+                                'http://stock.10jqka.com.cn/hks/ggdt_list/',
+                                'http://news.10jqka.com.cn/guojicj_list/',
+                                'http://yuanchuang.10jqka.com.cn/djsjdp_list/',
+                                'http://yuanchuang.10jqka.com.cn/mrnxgg_list/',
+                                'http://stock.10jqka.com.cn/chuangye/cybdt_list/',
+                                'http://news.10jqka.com.cn/fc_list/',
+                                'http://stock.10jqka.com.cn/jiepan_list/',
+                                'http://stock.10jqka.com.cn/bkfy_list/',
+                                'http://stock.10jqka.com.cn/bktt_list/',
+                                'http://stock.10jqka.com.cn/hks/hknews_list/',
+                                'http://stock.10jqka.com.cn/hks/ggyj_list/',
+                                'http://stock.10jqka.com.cn/usstock/gjgs_list/',
+                                'http://stock.10jqka.com.cn/usstock/zggxw_list/',
+                                'http://bond.10jqka.com.cn/zqzx_list/',
+                                'http://news.10jqka.com.cn/jrsc_list/',
+                                'http://goodsfu.10jqka.com.cn/futuresnews_list/',
+                                'http://goodsfu.10jqka.com.cn/spqh_list/',
+                                'http://invest.10jqka.com.cn/lczx_list/',
+                                'http://fe.10jqka.com.cn/whzx_list/',
+                                'http://news.10jqka.com.cn/cjkx_list/',
+                                'http://news.10jqka.com.cn/fortune_list/',
+                                'http://stock.10jqka.com.cn/hsdp_list/',
+                                'http://stock.10jqka.com.cn/tzjh_list/',
+                                'http://stock.10jqka.com.cn/ggqiquan_list/',
+                                'http://invest.10jqka.com.cn/sc_list/']
+
+        self.HEADERS = {
+            'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/95.0.4638.54 Safari/537.36'
+        }
+
+        self.SECTIONS_PAGES_DICT = dict()
+        self.SECTIONS_DICT = dict()
+        self.SECTIONS_DICT_W_KEY_WORDS = dict()
+        self.ARTICLES_URLS = []
+
+    def __create_full_link(self, start_url, list_of_links):
+        return [urllib.parse.urljoin(start_url, link) for link in list_of_links]
+
+    async def __get_soup_by_url(self, url, session, headers):
+        proxy = random.choice(proxy_list)
+        try:
+            async with session.get(url, headers=headers, proxy=proxy) as response:
+                response_text = await response.text()
+                print(url, f'with proxy {proxy} ', 'Access granted')
+                soup = BeautifulSoup(response_text, 'lxml')
+                return soup
+        except:
+            print(url, f'with proxy {proxy} ', 'Access denied')
+            return BeautifulSoup('', 'lxml')
+
+    async def __get_pages_for_section(self, section_url, session, headers):
+        section_soup = await self.__get_soup_by_url(section_url, session, headers)
+        try:
+            last_page_index = int(section_soup.find('a', {"class": "end"}).string)
+            pages_links_for_section = [f'{section_url}index_{index}.shtml' for index in range(1, last_page_index+1)]
+        except:
+            pages_links_for_section = []
+        full_pages_links_for_section = self.__create_full_link(section_url, pages_links_for_section)
+        self.SECTIONS_PAGES_DICT[section_url] = full_pages_links_for_section
+        return full_pages_links_for_section
+
+    async def __load_pages_for_sections(self, sections):
+        async with aiohttp.ClientSession() as session:
+            tasks = []
+            for section_url in sections:
+                task = asyncio.create_task(self.__get_pages_for_section(section_url, session, self.HEADERS))
+                tasks.append(task)
+            await asyncio.gather(*tasks)
+
+    async def __get_articles_for_page_in_section(self, page_link, section_url, session, headers):
+        page_soup = await self.__get_soup_by_url(page_link, session, headers)
+        page_article_links = [tag['href'] for tag in page_soup.find_all('a', {"class": "arc-cont"})]
+        full_page_article_links = self.__create_full_link(page_link, page_article_links)
+        self.SECTIONS_DICT[section_url] = self.SECTIONS_DICT[section_url].union(full_page_article_links)
+        return full_page_article_links
+
+    async def __load_articles_for_sections(self, sections):
+        async with aiohttp.ClientSession() as session:
+            tasks = []
+            for section_url in sections:
+                for page_link in self.SECTIONS_PAGES_DICT[section_url]:
+                    task = asyncio.create_task(
+                        self.__get_articles_for_page_in_section(page_link, section_url, session, self.HEADERS))
+                    tasks.append(task)
+            await asyncio.gather(*tasks)
+
+    async def __find_article_with_key_words(self, session, section, article, key_words_string):
+        proxy = random.choice(proxy_list)
+        try:
+            async with session.get(article, headers=self.HEADERS, proxy=proxy) as resp:
+                resp_text = await resp.text()
+                print(article, f'with proxy {proxy} ', 'Access granted')
+                if re.search(key_words_string, resp_text):
+                    print(f'found key words in {article}')
+                    self.ARTICLES_URLS.append(article)
+        except:
+            print(article, f'with proxy {proxy} ', 'Access denied')
+
+    async def __load_articles_with_key_words(self, sections, key_words_string):
+        async with aiohttp.ClientSession() as session:
+            tasks = []
+            for section in sections:
+                for article in self.SECTIONS_DICT[section]:
+                    task = asyncio.create_task(
+                        self.__find_article_with_key_words(session, section, article, key_words_string))
+                    tasks.append(task)
+            await asyncio.gather(*tasks)
+
+    def run(self, key_words_string):
+
+        with requests.Session() as s:
+
+            for section_url in self.list_of_section_urls:
+                self.SECTIONS_DICT[section_url] = set()
+                self.SECTIONS_PAGES_DICT[section_url] = []
+                self.SECTIONS_DICT_W_KEY_WORDS[section_url] = []
+
+            start_time = time.time()
+            asyncio.run(self.__load_pages_for_sections(self.list_of_section_urls))
+            asyncio.run(self.__load_articles_for_sections(self.list_of_section_urls))
+            asyncio.run(self.__load_articles_with_key_words(self.list_of_section_urls, key_words_string))
+
+            pd.DataFrame(self.ARTICLES_URLS, columns=['url']).to_csv(
+                '10jqka.csv',
+                index=False)
             end_time = time.time() - start_time
             print(f'Execution time: {end_time} seconds')
